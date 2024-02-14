7bb4d578f04f764d1f46b91c5c87180612e88f33
Gabriel Hernández <lsca_gabriel_hs@hotmail.com>
2024-02-14 17:26:24 -0600

    bn
    l

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const carousel = document.querySelector(".carousel"),
-firstImg = carousel.querySelectorAll("img")[0],
-arrowIcons = document.querySelectorAll(".wrapper i");
+    firstImg = carousel.querySelectorAll("img")[0],
+    arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
@@ -22,14 +22,14 @@ arrowIcons.forEach(icon => {
 
 const autoSlide = () => {
     // if there is no image left to scroll then return from here
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    if (carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
 
     positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
     let firstImgWidth = firstImg.clientWidth + 14;
     // getting difference value that needs to add or reduce from carousel left to take middle img center
     let valDifference = firstImgWidth - positionDiff;
 
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+    if (carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
     // if user is scrolling to the left
@@ -45,7 +45,7 @@ const dragStart = (e) => {
 
 const dragging = (e) => {
     // scrolling images/carousel to left according to mouse pointer
-    if(!isDragStart) return;
+    if (!isDragStart) return;
     e.preventDefault();
     isDragging = true;
     carousel.classList.add("dragging");
@@ -58,7 +58,7 @@ const dragStop = () => {
     isDragStart = false;
     carousel.classList.remove("dragging");
 
-    if(!isDragging) return;
+    if (!isDragging) return;
     isDragging = false;
     autoSlide();
 }
@@ -73,5 +73,37 @@ document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
 
 function onSubmit(token) {
-  document.getElementById("demo-form").submit();
+    document.getElementById("demo-form").submit();
+}
+
+// scrip para energia limpia, mostrar mas texto
+function showMoreText() {
+    // Obtén todos los párrafos con la clase 'additional-text'
+    var additionalTexts = document.querySelectorAll('.additional-text');
+    var btnText = document.getElementById("read-more-btn");
+    var allTextShown = true;
+
+    // Verifica el estado actual del botón para alternar la acción
+    if (btnText.textContent === "Leer más") {
+        // Muestra el siguiente párrafo oculto
+        for (var i = 0; i < additionalTexts.length; i++) {
+            if (additionalTexts[i].classList.contains('show')) {
+                continue;
+            } else {
+                additionalTexts[i].classList.add("show");
+                allTextShown = false; // No todos los textos están mostrados aún
+                break; // Solo muestra el primer párrafo oculto y luego sale del bucle
+            }
+        }
+        // Si después del bucle todos los textos están mostrados, cambia el texto del botón
+        if (allTextShown) {
+            btnText.textContent = "Mostrar menos";
+        }
+    } else {
+        // Oculta todos los párrafos
+        additionalTexts.forEach(function (text) {
+            text.classList.remove("show");
+        });
+        btnText.textContent = "Leer más";
+    }
 }
