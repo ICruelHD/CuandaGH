735b35e0cbc66ada5121618c6791906593faa8a4
Gabriel Hernández <lsca_gabriel_hs@hotmail.com>
2024-02-23 21:19:15 -0600

    .

--- js/script.js
+++ js/script.js
@@ -128,3 +128,13 @@ function toggleImages(selectedCarouselId) {
         selectedCarousel.style.display = 'flex';
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hash = window.location.hash;
+    if (hash && document.querySelector('.carousel-item' + hash)) {
+      const carousel = new bootstrap.Carousel(document.querySelector('#carrusel'));
+      const index = Array.from(document.querySelectorAll('.carousel-item')).findIndex(item => '#' + item.id === hash);
+      if (index !== -1) carousel.to(index);
+    }
+  });
+  
