e3c86321fa76274d0217f29cd2e7733583d86d59
Gabriel Hernández <lsca_gabriel_hs@hotmail.com>
2024-02-28 13:42:45 -0600

    .
    .

--- js/script.js
+++ js/script.js
@@ -138,3 +138,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   });
   
+  $(document).ready(function(){
+    $('.slick-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true,
+      draggable: true
+    });
+  });
+  
