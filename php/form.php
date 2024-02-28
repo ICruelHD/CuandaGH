b23a44d9c78f62d76505793a30f2eed72a12eae7
Gabriel Hernández <lsca_gabriel_hs@hotmail.com>
2024-02-28 15:31:06 -0600

    form
    Correccion del formulario

--- php/form.php
+++ php/form.php
@@ -2,12 +2,12 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $destinatarioCodigo = $_POST['asunto'];
 
-    $Asunto = $POST['asunto'];
+    $Asunto = $_POST['asunto'];
     $Empresa = $_POST['Empresa'];
     $Nombre = $_POST['Nombre'];
     $email = $_POST['email'];
-    $Estado = $POST['Estado'];
-    $Alcaldia = $POST['Alcaldia'];
+    $Estado = $_POST['Estado'];
+    $Alcaldia = $_POST['Alcaldia'];
     $telefono = $_POST['Telefono'];
     $mensaje = nl2br($_POST['mensaje']);
 
@@ -28,18 +28,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit;
     }
 
-    // Aquí iría el código para enviar el correo electrónico...
-    $exito = mail(
-        $destinatarioEmail, 
-        $email, 
-        $message = 'Empresa:' . $Empresa . ' 
-        Nombre:' . $Nombre . '
-	Correo:' . $email . '
-    Estado: ' . $Estado . '
-    Alcaldía: ' . $Alcaldia . '
-	No. telefonico: ' . $telefono . '
-	Mensaje: ' . $mensaje . '');
+    $message = "Empresa: $Empresa\n" .
+        "Nombre: $Nombre\n" .
+        "Correo: $email\n" .
+        "Estado: $Estado\n" .
+        "Alcaldía: $Alcaldia\n" .
+        "No. telefónico: $telefono\n" .
+        "Mensaje: $mensaje";
 
+    // Encabezados adicionales
+    $headers = "From: $email" . "\r\n" .
+        "Reply-To: $email" . "\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+    // Envía el correo electrónico
+    $exito = mail($destinatarioEmail, $Asunto, $message, $headers);
+    
     if ($exito) {
         echo "Mensaje enviado con éxito.";
     } else {
